8723f2cb46888698ef9bbd600e4af8a91a127098
Alysson Silveira <alysson.osilveira@gmail.com>
2019-11-24 00:21:53 -0500

    Add project files.

---
+++ DataAccess/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using static Food_Enforcement.Models.EF_Models;
+
+
+namespace Food_Enforcement.DataAccess
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        public DbSet<Result> Result { get; set; }
+    }
+}
